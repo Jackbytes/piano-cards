4d35d20acbf40bd763d76da41b9ac898a465048f
Jack <jackjharrington@icloud.com>
2021-06-05 18:50:10 +0100

    Better example data

--- src/Components/OnShowCard/OnShowCard.js
+++ src/Components/OnShowCard/OnShowCard.js
@@ -3,7 +3,7 @@ import OnShowInfo from "./OnShowInfo";
 
 const OnShowCard = (props) => {
   return (
-    <div className="w-full px-2 py-2 bg-white border-2 border-purple-100 shadow-lg rounded-xl">
+    <div className="w-full px-2 py-2 bg-white border-2 border-indigo-200 rounded-md shadow-xl">
       <OnShowTitle
         mainTitle={props.deckData.deckName}
         subTitle={props.deckData.deckType}
