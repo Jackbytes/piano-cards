db66dd456d5015b6cf70ab631df01128ae45e166
Jack <jackjharrington@icloud.com>
2021-06-05 17:50:09 +0100

    Basic Design now responsive.

--- src/Components/OnShowCard/OnShowInfo.js
+++ src/Components/OnShowCard/OnShowInfo.js
@@ -1,7 +1,7 @@
 const OnShowInfo = () => {
   return (
     <div>
-      <h1>Info Place Holder.</h1>
+      <h1 className="text-center md:text-left">Info Place Holder.</h1>
     </div>
   );
 };
--- src/Components/OnShowCard/OnShowTitle.js
+++ src/Components/OnShowCard/OnShowTitle.js
@@ -1,8 +1,8 @@
 const OnShowTitle = () => {
   return (
-    <div className="flex flex-row w-full place-items-center place-content-around border-red-50">
-      <h1 className="font-mono text-2xl underline">Deck</h1>
-      <h2 className="font-mono text-sm tracking-widest text-gray-500">
+    <div className="flex flex-row flex-wrap w-full place-items-center place-content-around border-red-50">
+      <h1 className="font-mono text-lg underline md:text-2xl ">Deck</h1>
+      <h2 className="font-mono text-xs tracking-widest text-gray-400 md:text-sm">
         Subdeck
       </h2>
     </div>
