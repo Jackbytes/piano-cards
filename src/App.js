591e4309c426e3a4fb6901558d94b51a466147b7
Jack <jackjharrington@icloud.com>
2021-06-05 18:21:40 +0100

    Put template card in middle. Looks nicer for now.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ let testDeck = require("./test_deck.json");
 
 function App() {
   return (
-    <div className="flex flex-col items-center w-1/3 mx-auto h-1/3">
+    <div className="flex flex-col items-center w-1/3 h-screen mx-auto place-content-center">
       <OnShowCard deckData={testDeck} />
     </div>
   );
