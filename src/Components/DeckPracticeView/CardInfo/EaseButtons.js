fc5e62b1f67a3576240b43df2e4ba8ba5bdd741b
Jack <jackjharrington@icloud.com>
2021-06-11 00:30:42 +0100

    Animated Ease Buttons

--- src/Components/DeckPracticeView/CardInfo/EaseButtons.js
+++ src/Components/DeckPracticeView/CardInfo/EaseButtons.js
@@ -1,13 +1,22 @@
 const EaseButtons = () => {
   return (
-    <div className="flex flex-row flex-wrap place-content-evenly">
-      <button className="px-4 py-1 text-white bg-green-500 border-2 border-pink-400 hover:bg-green-700 box-shadow-black">
+    <div className="flex flex-row flex-wrap place-content-evenly left-and-fade">
+      <button
+        type="button"
+        className="px-4 py-1 text-white bg-green-500 border-2 border-pink-400 hover:bg-green-700 box-shadow-black"
+      >
         EASY
       </button>
-      <button className="px-4 py-1 text-white bg-indigo-500 border-2 border-pink-400 hover:bg-indigo-700 box-shadow-black">
+      <button
+        type="button"
+        className="px-4 py-1 text-white bg-indigo-500 border-2 border-pink-400 hover:bg-indigo-700 box-shadow-black"
+      >
         OK
       </button>
-      <button className="px-4 py-1 text-white bg-red-500 border-2 border-pink-400 hover:bg-red-700 box-shadow-black">
+      <button
+        type="button"
+        className="px-4 py-1 text-white bg-red-500 border-2 border-pink-400 hover:bg-red-700 box-shadow-black"
+      >
         HARD
       </button>
     </div>
